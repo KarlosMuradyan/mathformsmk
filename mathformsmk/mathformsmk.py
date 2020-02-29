96b011680d651e21b5e2768fbf0c59b10280423b
Karlos Muradyan <carlosmuradyan2@gmail.com>
2020-02-29 13:28:38 -0800

    First working version

---
+++ mathformsmk/mathformsmk.py
@@ -0,0 +1,25 @@
+def log_sum_exp_stable(z):
+    """
+    Computes cross entropy loss. More stable implementation.
+    
+    Parameters
+    ----------
+    z: numpy.ndarray containing outputes of linear model
+    
+    Returns
+    -------
+    float : loss
+    
+    Examples:
+    --------
+    >>> log_sum_exp_stable(np.ones(100))
+    5.605170185988092
+    """
+    a = np.max(z)
+    return a + np.log(np.sum(np.exp(z-a)))
+
+def log_1_plus_exp_safe(z):
+    if z > 100:
+        return z
+    else:
+        return np.log(1+np.exp(z))
